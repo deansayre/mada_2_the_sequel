491b6042036e2ecaebdcb77f06f62de19e81e215
Dean Sayre <omp2@cdc.gov>
2021-11-07 12:53:57 -0500

    Adding functions to translate factor labels (HFS)

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,9 +1,10 @@
 
-# loads the most recent IPC  data and cleans strings
+# loads the most recent survey data and cleans strings
 
 path_to_folder <- here::here("data")
 bases <- file.info(list.files(path_to_folder,full.names = TRUE))
 path_to_base <- rownames(bases)[which.max(bases$mtime)]
+
 data <- rio::import(path_to_base) %>% 
   linelist::clean_data(guess_dates = F)
 
@@ -12,7 +13,6 @@ data_c <- data %>%
        consent_signe1, result_final1, statut_mng2, consent_signe2, result_final2, 
        statut_mng3, consent_signe3, result_final3, materiau_toit,
        .direction = "down")
-
 
 
 hh_fact <- data_c %>% 
@@ -51,6 +51,89 @@ indiv <- data_c %>%
          across(all_of(indiv_fact), ~factor(.)), 
          across(contains('heure'), ~ lubridate::hm(str_replace(., "_", ":")))) %>% 
   select(-c(nom_indiv, prenom_indiv))
+
+Hmisc::describe(indiv$csb_rattach)
+
+fct_label <- function(x,
+                      label) {
+  
+#  if (!is.factor(x))
+#    stop("Not a factor variable")
+  
+  attr(x, "label") <- label
+  
+  x
+  
+}
+
+
+code1 <- rio::import("exitinterview.xlsx", which = "choices") %>% 
+  linelist::clean_variable_names()
+
+codebook <- rio::import("codebook.xlsx")
+rosetta_stone <- rio::import("exitinterview.xlsx", which = "survey") %>% 
+  filter(str_detect(type, "select_one") | str_detect(type, "select_multiple")) %>% 
+  select(name, type) %>% 
+  mutate(type = str_remove(type, "select_one "), 
+         type = str_remove(type, "select_multiple "))
+
+dummy <- code1 %>% 
+  right_join(rosetta_stone, by = c("list_name" = "type")) %>% 
+  distinct(list_name, name.x, name.y, .keep_all = T) %>% 
+  arrange(name.y) %>% 
+  rename_with(~ "french", .cols = contains("fr")) %>% 
+  rename_with(~ "english", .cols = contains("eng"))
+
+
+facade <- function(df, langauge){
+  df1 <- matchmaker::match_df(df,
+             dictionary = dummy,
+             from = "name.x",
+             to = {{langauge}},
+             by = "list_name")
+  return(df1)
+}
+
+get_codes <- function(df, var, language){
+  df1 <- df %>% 
+    select(var) %>% 
+    mutate(var1 = var) %>% 
+    matchmaker::match_df(.,
+                       dictionary = dummy,
+                       from = "name.x",
+                       to = {{langauge}},
+                       by = "list_name") %>% 
+    mutate(out = paste0(var1, " - ", var))
+  return(df1$out)
+}
+
+trial <- rio::import("data_example.xlsx")
+
+a <- names(indiv) %>% 
+  as_tibble()
+
+dummy <- indiv %>% 
+  select(csb_rattach) %>% 
+  fct_label(., codebook$label)
+
+
+
+levels(dummy$csb_rattach)
+
+dummy2 <- dummy %>% 
+  mutate(csb_rattach = lfactor(csb_rattach, levels = 1:37, 
+                               labels = codebook$label), 
+         csb_rattach = fct_relevel(csb_rattach, "ee", "z"))
+
+dummy3 <- dummy2 %>% 
+  filter(csb_rattach == 26)
+
+llevels(dummy2$csb_rattach)
+
+attr(dummy2$csb_rattach, "label")
+
+ggplot(dummy2, aes(x = csb_rattach)) + 
+  geom_bar()
 
 
 rm(hh_fact, indiv_fact)
